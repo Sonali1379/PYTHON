2feb81ac732d2dda93e307b3a8814d014d71b27d
nilesh-p-aarchik <swainsonali1379@gmail.com>
2026-07-26 13:50:07 +0530

    Session-4

---
+++ Assignment/Django/SESSION_4/foodiespot/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Restaurant(models.Model):
+    cuisine = models.ForeignKey(Cuisine,on_delete=models.CASCADE,default=True)
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=70)
+    rating = models.FloatField()
+
